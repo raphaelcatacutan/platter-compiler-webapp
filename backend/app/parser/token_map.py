5859152aaa092f7566e20a35810e0f6aeeb16ea1
Jieco Lao <jiecoandres.lao07@gmail.com>
2025-11-22 23:48:52 +0800

    added dictionaries for parser

--- backend/app/parser/token_map.py
+++ backend/app/parser/token_map.py
@@ -1,92 +1,103 @@
 TOKEN_MAP = {
-  # Data Types
-  "CHARS": "chars",
-  "FLAG": "flag",
-  "PIECE": "piece",
-  "SIP": "sip",
-  "TABLE": "table",
-  # Logical Operators
-  "AND": "and",
-  "NOT": "not",
-  "OR": "or",
-  # Conditional Statements
-  "ALT": "alt",
-  "CHECK": "check",
-  "CHOICE": "choice",
-  "INSTEAD": "instead",
-  "MENU": "menu",
-  "USUAL": "usual",
-  # Looping Statements
-  "ORDER": "order",
-  "PASS": "pass",
-  "REPEAT": "repeat",
-  # Jump Statements
-  "NEXT": "next",
-  "STOP": "stop",
-  "SERVE": "serve",
-  # Built-in Recipes
-  "APPEND": "append",
-  "BILL": "bill",
-  "COPY": "copy",
-  "CUT": "cut",
-  "FACT": "fact",
-  "MATcHES": "matches",
-  "POW": "pow",
-  "RAND": "rand",
-  "REMOVE": "remove",
-  "REVERSE": "reverse",
-  "SEARCH": "search",
-  "SIZE": "size",
-  "SORT": "sort",
-  "SQRT": "sqrt",
-  "TAKE": "take",
-  "TOCHARS": "tochars",
-  "TOPIECE": "topiece",
-  "TOSIP": "tosip",
-  # Others
-  "DOWN": "down",
-  "OF": "of",
-  "PREPARE": "prepare",
-  "START": "start",
-  "UP": "up",
-  # Arithmetic Operators
-  "PLUS": "+",
-  "SUB": "-",
-  "MUL": "*",
-  "DIV": "/",
-  "MOD": "%",
-  # Relational Operators
-  "G_THAN": ">",
-  "L_THAN": "<",
-  "2EQUAL": "==",
-  "LEQUAL": "<=",
-  "GEQUAL": ">=",
-  "NEQUAL": "!=",
-  # Assignment Operators
-  "EQUAL": "=",
-  "PLUSEQ": "+=",
-  "SUBEQ": "-=",
-  "MULEQUAL": "*=",
-  "DIVEQUAL": "/=",
-  "MODEQUAL": "%=",
-  # Other Symbols
-  "LBRACE": "{",
-  "RBRACE": "}",
-  "LPAREN": "(",
-  "RPAREN": ")",
-  "LSQB": "[",
-  "RSQB": "]",
-  "COMMA": ",",
-  "SEMICOLON": ";",
-  "COLON": ":",
-  "NEWLINE": "newline",
-  "TAB": "tab",
-  "SPACE": "space",
-  # Others
-  "PIECE_LIT": "piece_lit",
-  "SIP_LIT": "sip_lit",
-  "CHARS_LIT": "chars_lit",
-  "ID": "id",
-  "COMMENT_SINGLE": "comment_single",
-  "COMMENT_MULTI": "comment_multi"
+  """ Data Types """
+  "chars": "chars",
+  "flag": "flag",
+  "piece": "piece",
+  "sip": "sip",
+  "table": "table",
+
+  """ Logical Operators """
+  "and": "and",
+  "not": "not",
+  "or": "or",
+  
+  """ Conditional Statements """
+  "alt": "alt",
+  "check": "check",
+  "choice": "choice",
+  "instead": "instead",
+  "menu": "menu",
+  "usual": "usual",
+
+  """ Looping Statements """
+  "order": "order",
+  "pass": "pass",
+  "repeat": "repeat",
+
+  """ Jump Statements """
+  "next": "next",
+  "stop": "stop",
+  "serve": "serve",
+
+  """ Built-in Recipes """
+  "append": "append",
+  "bill": "bill",
+  "copy": "copy",
+  "cut": "cut",
+  "fact": "fact",
+  "matches": "matches",
+  "pow": "pow",
+  "rand": "rand",
+  "remove": "remove",
+  "reverse": "reverse",
+  "search": "search",
+  "size": "size",
+  "sort": "sort",
+  "sqrt": "sqrt",
+  "take": "take",
+  "tochars": "tochars",
+  "topiece": "topiece",
+  "tosip": "tosip",
+
+  """ Others """
+  "down": "down",
+  "of": "of",
+  "prepare": "prepare",
+  "start": "start",
+  "up": "up",
+  
+  """ Arithmetic Operators """
+  "+": "+",
+  "-": "-",
+  "*": "*",
+  "/": "/",
+  "%": "%",
+
+  """ Relational Operators """
+  ">": ">",
+  "<": "<",
+  "==": "==",
+  "<=": "<=",
+  ">=": ">=",
+  "!=": "!=",
+
+  """ Assignment Operators"""
+  "=": "=",
+  "+=": "+=",
+  "-=": "-=",
+  "*=": "*=",
+  "/=": "/=",
+  "%=": "%=",
+
+  """ Other Symbols """
+  "{": "{",
+  "}": "}",
+  "(": "(",
+  ")": ")",
+  "[": "[",
+  "]": "]",
+  ",": ",",
+  ";": ";",
+  ":": ":",
+  "newline": "newline",
+  "tab": "tab",
+  "space": "space",
+  
+  """ Others """
+  "piece_lit": "piece_lit",
+  "sip_lit": "sip_lit",
+  "chars_lit": "chars_lit",
+  "id": "id",
+  "comment_single": "comment_single",
+  "comment_multi": "comment_multi"
 }
